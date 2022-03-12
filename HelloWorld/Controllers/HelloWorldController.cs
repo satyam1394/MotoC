ad4042e08e09b4ac9af4fa43dfa9f810ac2f0c9d
satyam1394 <satyam1394@gmail.com>
2022-03-12 08:35:01 +0000

    Add files via upload

---
+++ HelloWorld/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloWorld.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWorld.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            string data = new HelloWorldService().GetHelloWorld();
+            return data;
+        }
+    }
+}
